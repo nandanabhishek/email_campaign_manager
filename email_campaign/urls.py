e71d07e0d5451655c3e59b6984d834f7ee1e70a9
Abhishek Nandan <abhishek.nandan@perficient.com>
2023-09-09 14:15:09 +0530

    added views to save the user entered campaign to database and render the list of all campaigns.

--- email_campaign/urls.py
+++ email_campaign/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('add_subscriber/', views.add_subscriber, name='add_subscriber'),
     path('add_campaign/', views.add_campaign, name='add_campaign'),
     path('subscriber_list/', views.subscriber_list, name='subscriber_list'),
+    path('campaign_list/', views.campaign_list, name='campaign_list'),
 ]
--- email_campaign/views.py
+++ email_campaign/views.py
@@ -4,6 +4,14 @@ from django.shortcuts import render, redirect
 # Create your views here. 
 from .models import Subscriber, Campaign
 
+def campaign_list(request):
+    campaigns = Campaign.objects.all()
+    return render(request, 'campaign_list.html', {'campaigns': campaigns})
+
+def subscriber_list(request):
+    subscribers = Subscriber.objects.all()
+    return render(request, 'subscriber_list.html', {'subscribers': subscribers})
+
 def add_subscriber(request):
     if request.method == 'POST':
         email = request.POST.get('email')
@@ -16,13 +24,28 @@ def add_subscriber(request):
 def add_campaign(request):
     if request.method == 'POST':
         # Handle campaign creation here
-        # Ensure proper validation and saving of Campaign objects
-        pass
-    return render(request, 'add_campaign.html')
+        subject = request.POST.get('subject')
+        preview_text = request.POST.get('preview_text')
+        article_url = request.POST.get('article_url')
+        html_content = request.POST.get('html_content')
+        plain_text_content = request.POST.get('plain_text_content')
+        published_date = request.POST.get('published_date')  # You may need to parse this date
 
-def subscriber_list(request):
-    subscribers = Subscriber.objects.all()
-    return render(request, 'subscriber_list.html', {'subscribers': subscribers})
+        # Create and save the Campaign object
+        campaign = Campaign(
+            subject=subject,
+            preview_text=preview_text,
+            article_url=article_url,
+            html_content=html_content,
+            plain_text_content=plain_text_content,
+            published_date=published_date,
+        )
+        campaign.save()
+
+        # Redirect to a success page or wherever needed
+        return redirect('campaign_list')  # Redirect to the campaign list view
+        
+    return render(request, 'add_campaign.html')
 
 import concurrent.futures
 from django.core.mail import send_mail
@@ -43,5 +66,5 @@ def send_daily_campaign_parallel():
         executor.map(send_campaign, campaigns)
 
 # to test the working of send_daily_campaign_parallel()
-send_daily_campaign_parallel()
+# send_daily_campaign_parallel()
 
